4d20c134cdde3269e5817f7b369805984ec368e7
Cameroonparr <106214642+Cameroonparr@users.noreply.github.com>
2026-01-25 18:03:07 +1300

    Update organizer_script.py
    Added functionality - Create new directories as required.

--- organizer script/organizer_script.py
+++ organizer script/organizer_script.py
@@ -1,7 +1,19 @@
 import shutil
 from pathlib import Path
 
-# 1. Setup Path
+#Create directories 
+def dir_create(dir_name):
+    try:
+        dir_name.mkdir()
+        print(f"'{dir_name}' created.")
+    except FileExistsError:
+        print(f"'{dir_name}' already exists.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{dir_name}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# 1. Setup Path - Wherever script is 
 script_location = Path(__file__).resolve().parent
 print(f"Script location: {script_location}")
 print("*"*20)
@@ -11,12 +23,20 @@ directories = []
 #Contains just files & extensions, no dirs 
 master_dict = {}
 
+#Create folders for orginization
+
 image_dir = script_location / "images"
-misc_dir = script_location / "misc"
 power_points_dir = script_location / "power_points"
 word_docs_dir = script_location / "word_docs"
 
+file_types = [image_dir,power_points_dir,word_docs_dir]
+
+#Create directories to sort files into
+for file_type in file_types:
+    dir_create(file_type)
+
 # 2. SCAN: Use .iterdir() to get Path objects
+
 for item in script_location.iterdir():
     
     # IDENTIFY: Check if it's a file or directory
@@ -46,13 +66,20 @@ for key, value in master_dict.items():
     if value == ".pptx":
         file_location = script_location / (key+value)
         shutil.move(file_location,power_points_dir )
-        
     if value == ".docx":
         file_location = script_location / (key+value)
         shutil.move(file_location, word_docs_dir)
-
     if value == ".jfif":
         file_location = script_location / (key+value)
-        shutil.move(file_location,image_dir)
+        shutil.move(file_location,image_dir) 
+    if value == ".jpg":
+        file_location = script_location / (key+value)
+        shutil.move(file_location,image_dir) 
+    if value == ".png":
+        file_location = script_location / (key+value)
+        shutil.move(file_location,image_dir)     
+
+
         
+
 
